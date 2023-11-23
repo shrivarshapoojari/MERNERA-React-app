9648965e962c67eb5219484f44594976f5d25c9c
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2023-11-23 22:51:13 +0530

    axios

--- Frontend/src/config/axiosInstance.js
+++ Frontend/src/config/axiosInstance.js
@@ -1,5 +1,5 @@
  import axios from "axios"
-const BASE_URL="http://localhost:5000/api"
+const BASE_URL="https://mernlearnapi3.onrender.com/api"
 
  const axiosInstance=axios.create()
 axiosInstance.defaults.baseURL=BASE_URL;
